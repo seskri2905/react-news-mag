649820e8bf2a998760f9332c63aa3eb0c3807549
Sesha Krishnan <mseshu29@gmail.com>
2026-01-28 16:30:00 +0530

    Changed the code in NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -17,17 +17,18 @@ const NewsBoard = ({ category }) => {
       <h2 className="text-center">
         Latest <span className="badge bg-danger">News</span>
       </h2>
-      {articles.map((news, index) => {
-        return (
-          <NewsItem
-            key={index}
-            title={news.title}
-            description={news.description}
-            src={news.urlToImage}
-            url={news.url}
-          />
-        );
-      })}
+      {articles.length > 0 &&
+        articles.map((news, index) => {
+          return (
+            <NewsItem
+              key={index}
+              title={news.title}
+              description={news.description}
+              src={news.urlToImage}
+              url={news.url}
+            />
+          );
+        })}
     </div>
   );
 };
